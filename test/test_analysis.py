85332f867e15d7cbf7734aa24354f071cd97d778
Emir Karayagiz <emir.karayagiz@fen.ktu.edu.tr>
2025-07-04 16:38:52 +0300

    feat: Update file path and column name for topic analysis in test script

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -4,17 +4,18 @@ print(dir(nmf_standalone))
 emj_map = nmf_standalone.EmojiMap()
 
 
-file_path = "../veri_setleri/APPSTORE_APP_REVIEWSyeni_yeni.csv"
-column = "REVIEW"
+file_path = "../veri_setleri/PLAYSTORE_APP_REVIEWS.csv"
+column = "REVIEW_TEXT"
 nmf_standalone.run_topic_analysis(filepath=file_path,
                                   column=column,
+                                  separator=';',
                                   language="TR",
                                   tokenizer_type="bpe",
                                   lemmatize=True,
                                   generate_wordclouds=True,
-                                  topic_count=8,
+                                  topic_count=5,
                                   words_per_topic=15,
                                   emoji_map=emj_map,
-                                  word_pairs_out=False,
-                                  separator='|')
+                                  word_pairs_out=True
+                                  ,nmf_method="opnmf")
 
