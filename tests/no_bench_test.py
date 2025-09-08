e96447a2d29af0b64e4b0f37d38cf919c5e04600
Emir Karayagiz <emir.karayagiz@fen.ktu.edu.tr>
2025-09-08 07:56:22 +0300

    refactor: Update no_bench_test.py to change input file and column for topic analysis

--- tests/no_bench_test.py
+++ tests/no_bench_test.py
@@ -1,25 +1,26 @@
 import manta
 
-file_path = "../veri_setleri/bbc_news.csv"
-column = "text"
+if __name__ == '__main__':
+    file_path = "../veri_setleri/covid_abstracts.csv"
+    column = "abstract"
 
-result = manta.run_topic_analysis(
-    filepath=file_path,
-    column=column,
-    separator=",",
-    language="EN",
-    tokenizer_type="bpe",
-    lemmatize=True,
-    generate_wordclouds=True,
-    topic_count=10,
-    words_per_topic=15,
-    emoji_map=True,
-    word_pairs_out=False,
-    nmf_method="nmf", # "nmf" or "nmtf" or "pnmf"
-    filter_app=False,
-    data_filter_options = {
-        "filter_app_country": "TR",
-        "filter_app_country_column": "REVIEWER_LANGUAGE",
-    },
-    save_to_db=False
-)
+    result = manta.run_topic_analysis(
+        filepath=file_path,
+        column=column,
+        separator=",",
+        language="EN",
+        lemmatize=True,
+        topic_count=10,
+        words_per_topic=15,
+        nmf_method="nmf", # "nmf" or "nmtf" or "pnmf"
+        filter_app=False,
+        data_filter_options = {
+            "filter_app_country": "TR",
+            "filter_app_country_column": "REVIEWER_LANGUAGE",
+        },
+        emoji_map=True,
+        generate_wordclouds=True,
+        save_to_db=False,
+        word_pairs_out=False,
+        topic_distribution=True,
+    )
